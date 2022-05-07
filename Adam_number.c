e8f1d9bdcd7710472d381eb2e9df079d85fddb6f
21A91A1203 <98650226+21A91A1203@users.noreply.github.com>
2022-05-07 07:18:37 +0530

    #include<stdio.h> int main() {     int n,sum=0,d,r,res,sq,rev=0;     scanf("%d",&n);     res=n*n;       while(n>0)      {          d=n%10;          rev=(rev*10)+d;          n=n/10;      }       sq=rev*rev;     while(sq>0)     {         r=sq%10;         sum=(sum*10)+r;         sq=sq/10;      }
        // sq=rev*rev;
         if(res==sum)
         {
             printf("True");
         }
         else
         {
             printf("False");
         }
    }

--- Adam_number.c
+++ Adam_number.c
@@ -1,29 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,x=0,y,a,b,c=0;
+    int n,sum=0,d,r,res,sq,rev=0;
     scanf("%d",&n);
-    y=n*n;
-    while(n>0)
+    res=n*n;
+      while(n>0)
+     {
+         d=n%10;
+         rev=(rev*10)+d;
+         n=n/10;
+     }
+      sq=rev*rev;
+    while(sq>0)
     {
-        r=n%10;
-        x=x*10+r;
-        n=n/10;
-    }
-a=x*x;
-while(a>0)
-{
-    b=a%10;
-    c=c*10+b;
-    a=a/10;
-}
-if(y==c)
-{
-    printf("True");
-}
-else
-{
-    printf("False");
-}
-return 0;
+        r=sq%10;
+        sum=(sum*10)+r;
+        sq=sq/10;
+     }
+   
+    // sq=rev*rev;
+     if(res==sum)
+     {
+         printf("True");
+     }
+     else
+     {
+         printf("False");
+     }
 }
